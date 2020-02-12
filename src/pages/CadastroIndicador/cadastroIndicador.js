5e01ab43e2b3e10630a63fdc17166c78ab137724
Robson Silveira <robson.silveira@gx2.local>
2020-02-12 19:59:09 -0300

    Implementação de cadastro de indicador

--- src/pages/CadastroIndicador/cadastroIndicador.js
+++ src/pages/CadastroIndicador/cadastroIndicador.js
@@ -1,10 +1,49 @@
 import React, { Component } from 'react';
 import { ContainerMain } from './styles';
+import api from 'services';
 
 export default class CadastroIndicador extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      descricao: '',
+    };
+  }
+
+  handleDescricao = e => {
+    const { descricao } = this.state;
+    this.setState({ descricao: e.target.value });
+    console.log(descricao);
+  };
+
+  handleSend = e => {
+    const { descricao } = this.state;
+
+    if (descricao.length != 0) {
+      this.handleRequest();
+      console.log('Entrei');
+    } else {
+      alert('Todos os campos devem estar preenchidos.');
+    }
+  };
+
+  handleRequest() {
+    const { descricao } = this.state;
+
+    const response = api
+      .post('/indicadores/', {
+        descricao: descricao,
+      })
+      .then(
+        response => {
+          console.log(response);
+        },
+        error => {
+          console.log(error);
+        }
+      );
+    console.log('teste', response);
+    alert('Indicador cadastrado com sucesso.');
   }
 
   render() {
@@ -14,8 +53,13 @@ export default class CadastroIndicador extends Component {
           <h1> Cadastro de Indicador</h1>
           <div className="divForm">
             <form>
-              <input type="text" placeholder="Descrição" />
-              <button>Salvar</button>
+              <input
+                type="text"
+                placeholder="Descrição *"
+                onChange={this.handleDescricao}
+                //required
+              />
+              <button onClick={this.handleSend}>Enviar</button>
             </form>
           </div>
         </ContainerMain>
